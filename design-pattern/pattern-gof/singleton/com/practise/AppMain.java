45c4fa3cb3140f833f2916a2aacd9ceb0d954aa9
dilhan <dilhan.ariyarathna@iihsolutions.com>
2024-08-13 19:26:14 +0530

    singleton practise

--- design-pattern/pattern-gof/singleton/com/practise/AppMain.java
+++ design-pattern/pattern-gof/singleton/com/practise/AppMain.java
@@ -1,2 +1,28 @@
-package com.practise;public class AppMain {
+package com.practise;
+
+import java.io.*;
+
+public class AppMain {
+
+    public static void main(String arg[]) throws IOException, ClassNotFoundException {
+        System.out.println("Hellow");
+        try {
+            DateUtil.getInstance().clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println("exception : " + e.getMessage());
+        }
+
+        DateUtil dateUtil = DateUtil.getInstance();
+        ObjectOutputStream  oos = new ObjectOutputStream(new FileOutputStream(new File("C:\\Users\\Other\\Documents\\design-patterns\\dateUtil.ser")));
+        oos.writeObject(dateUtil);
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("C:\\Users\\Other\\Documents\\design-patterns\\dateUtil.ser")));
+        DateUtil dateUtil2 = (DateUtil) ois.readObject();
+
+        oos.close();
+        ois.close();
+
+        // this will true since we have resolved the serialization issue.
+        System.out.println(dateUtil == dateUtil2);
+    }
 }
--- design-pattern/pattern-gof/singleton/com/practise/DateUtil.java
+++ design-pattern/pattern-gof/singleton/com/practise/DateUtil.java
@@ -1,2 +1,34 @@
-package com.practise;public class DateUtil {
+package com.practise;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class DateUtil implements Cloneable, Serializable {
+    /** volatile variable containing values are visible to other threads only when write complete **/
+    private static volatile DateUtil dateUtil;
+    private static final long serialVersionId = 1L;
+    private DateUtil(){
+    }
+
+    public static DateUtil getInstance() {
+        if(dateUtil == null) {
+            synchronized (DateUtil.class){
+                if(dateUtil == null) {
+                    dateUtil = new DateUtil();
+                }
+            }
+        }
+        return  dateUtil;
+    }
+
+    /** avoid cloning of this singleton **/
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException("Clone not supported exception.");
+    }
+
+    /** fix serialization problume (when object are desirialize from disk, single instance cant be different.) **/
+    public Object readResolve() {
+        return dateUtil;
+    }
 }
